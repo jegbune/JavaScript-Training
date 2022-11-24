827c07c87bd9ba598012087311e19f1e00925996
Jeremiah Ikechukwu <jegbune1@gmail.com>
2022-11-25 00:17:42 +0100

    while-loops if statement

---
+++ logical_operator.js
@@ -0,0 +1,25 @@
+var myAge = 2;
+
+if (myAge >= 18 && myAge <= 30){
+
+    document.write("You can come, you cool dude");
+
+} else{
+
+    document.write("you aint coming");
+
+}
+
+
+var myAge = 20;
+
+if (myAge < 18 || myAge > 30 || myAge == 25){
+
+    document.write("you aint coming");
+
+} else{
+
+    document.write("You can come, you cool dude");
+
+}
+
---
+++ while-loops.js
@@ -0,0 +1,18 @@
+let age = 5;
+
+ while (age < 10){
+
+     console.log("Your age is less than 10)");
+     age++
+}
+
+ document.write("you are now over 10");
+
+
+// for(age = 0; age < 10; age++){
+
+//     console.log("Your age is $(age)");
+    
+// }
+
+// document.write("you are now over 10");

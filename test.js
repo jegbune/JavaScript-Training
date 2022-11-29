04b4e66bf4591fd2c8206f0971c0a54dbf978fa5
Jeremiah Ikechukwu <jegbune1@gmail.com>
2022-11-29 15:36:08 +0100

    javaScript lesson on for loop

--- test.js
+++ test.js
@@ -1,9 +1,32 @@
-var links = document.getElementsByTagName("a")
+ var links = document.getElementsByTagName("a");
 
-for (i = 1; i < links.length; i++) {
+for (i = 1; i <= links.length; i++) {
 
-    console.log("This is link number" + i);
+    console.log("This is link number " + i);
 
 }
 
-document.write("all links now looped")
+// document.write("all links now looped")
+//var i =0
+// for (var i = 0; i < 10; i++) {
+
+//     if(i === 5 || i === 3) {
+//         continue;
+//     }
+//     console.log(i);
+
+//     if(i === 7) {
+
+//         break;
+//     }
+// }
+
+// console.log("I have broken out of the loop")
+
+var links = document.getElementsByTagName("a");
+
+for (i = 0; i <= links.length; i++) {
+
+    links[i].className = "link-" +i;
+
+}

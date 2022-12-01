77f9e66be473b9ec7c3889f029f308a24c190e41
Jeremiah Ikechukwu <jegbune1@gmail.com>
2022-12-01 23:15:00 +0100

    javascript lesson on math

--- test.js
+++ test.js
@@ -23,10 +23,22 @@
 
 // console.log("I have broken out of the loop")
 
-var link = document.getElementsByTagName("a");
-var i = 0;
-while (i <= link.length) {
+// var link = document.getElementsByTagName("a");
+// var i = 0;
+// while (i <= link.length) {
 
-    link[i].className = "link-" +i;
-    i++
-}
+//     link[i].className = "link-" +i;
+//     i++
+// }
+var a = 5;
+var b = 5;
+console.log(a + b);
+console.log(typeof (a + b));
+console.log(Math.round(7.5));
+console.log(Math.floor(7.5));
+console.log(Math.ceil(7.1));
+console.log(Math.max(7,5,10,8,10.1));
+console.log(Math.PI);
+
+
+

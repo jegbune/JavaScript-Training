58274225a9bdce23a29c52eed36510a2e7369ed4
Jeremiah Ikechukwu <jegbune1@gmail.com>
2022-12-07 14:30:50 +0100

    javascript operators

---
+++ operator.js
@@ -0,0 +1,15 @@
+    let a = 3;
+    let x = (100 + 50) * a;
+    document.getElementById("for").innerHTML = x;
+
+    let y = 10;
+    y += 5;
+    document.getElementById("for1").innerHTML = x;
+
+    let name = "korede";
+    let age = "26";
+
+    let nameAge = name + age;
+
+    document.getElementById("for2").innerHTML= nameAge;
+

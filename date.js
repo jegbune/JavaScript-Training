7f0e7131aae49d4aa834cca656bb1596b5ae40fb
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-04 22:10:00 +0100

    javascript lesson on date.js

--- date.js
+++ date.js
@@ -1,8 +1,18 @@
 let d = new Date();
 console.log(d);
-let da = new Date("october 12, 2014 11:30:00");
-console.log(da.toDateString());
-console.log(da.toUTCString());
-console.log(da.toISOString());
-msec = Date.parse(d);
-console.log(msec);
+// let da = new Date("october 12, 2014 11:30:00");
+// console.log(da.toDateString());
+// console.log(da.toUTCString());
+// console.log(da.toISOString());
+// msec = Date.parse(d);
+// console.log(msec);
+let month = ["January","Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+console.log(d.getFullYear())
+console.log(d.getMonth() + 1)
+console.log(month[d.getMonth() ])
+console.log(d.getDate())
+console.log(d.getHours())
+console.log(d.getMinutes())
+console.log(d.getSeconds())
+
+

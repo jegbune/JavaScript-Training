865ea8ee755bdc50cd4e6a0f1de57bc01189d908
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-04 23:36:02 +0100

    Javascript lesson on date

---
+++ Math.js
@@ -0,0 +1,13 @@
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log("4.46 is rounded off to integer " + Math.round(4.46))
+console.log("4.2 is rounded up to integer " + Math.ceil(4.2))
+console.log("5.9 is rounded down to integer " + Math.floor(5.9))
+console.log("25.9 is truncate toward zero " + Math.trunc(25.9))
+console.log("Math.sign(x) returns if x is negative, null or positive: " + Math.sign(-9))
+console.log("The Square of 9 is " + Math.pow(9,2))
+console.log("The cube of 3 is " + Math.pow(3,3))
+console.log("The Square root of 625 is " + Math.sqrt(625))
+console.log("The Absolute value of -4.2 is " + Math.abs(-4.2))
+console.log("The sine value of 90 degrees is " + Math.sin(90 * Math.PI / 180))
+console.log("The sine value of 9 degrees is " + Math.sin(9 * Math.PI / 180))
--- date.js
+++ date.js
@@ -7,12 +7,21 @@ console.log(d);
 // msec = Date.parse(d);
 // console.log(msec);
 let month = ["January","Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-console.log(d.getFullYear())
-console.log(d.getMonth() + 1)
-console.log(month[d.getMonth() ])
-console.log(d.getDate())
-console.log(d.getHours())
-console.log(d.getMinutes())
-console.log(d.getSeconds())
+// console.log(d.getFullYear())
+// console.log(d.getMonth() + 1)
+// console.log(month[d.getMonth() ])
+// console.log(d.getDate())
+// console.log(d.getHours())
+// console.log(d.getMinutes())
+// console.log(d.getSeconds())
+
+d.setFullYear(2022);
+d.setMonth(4)
+console.log(d)
+
+// console.log(d.setDate(28/9/2000))
+// console.log(d.setHours(13))
+// console.log(d.setMinutes(50))
+// console.log(d.setSeconds(20))
 
 

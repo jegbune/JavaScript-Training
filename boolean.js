19100021303d25ff69e046a2c7bbb1e79f2cb6e9
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-11 07:43:09 +0100

    javascript recap on if statement

--- boolean.js
+++ boolean.js
@@ -1,3 +1,6 @@
-console.log(Boolean(5>8));
-console.log(10>3)
-console.log(Boolean(false))
+// console.log(Boolean(5>8));
+// console.log(10>3)
+// console.log(Boolean(false))
+ let meat = "true"
+ console.log(Boolean(7==7))
+ console.log(Boolean("meat"))

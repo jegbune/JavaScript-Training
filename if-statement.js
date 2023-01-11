f5ac88a48529ffc1dadd6f72387e0d641d41545a
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-11 07:43:44 +0100

    javascript recap on if statement

---
+++ if-statement.js
@@ -0,0 +1,28 @@
+// let youLikeMeat = false
+// myNum = 8
+
+// if(youLikeMeat) {
+
+//     console.log("You like meat")
+
+// }
+// else{
+// console.log("You hate meat")
+// }
+let myAge =900;
+if (myAge > 30) {
+
+    console.log(" you are over 30")
+
+}
+else if( myAge > 20) {
+
+    console.log(" you are over 20")
+
+}
+else if(myAge > 10) {
+    console.log(" you are over 10!")
+}
+else{
+    console.log ("you are not over 10!")
+}

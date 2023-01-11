020dab96492c67b74a93570e180d1993c13d0afc
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-11 16:43:20 +0100

    update on while loop

--- while-loops.js
+++ while-loops.js
@@ -1,20 +1,35 @@
-var age = 3;
+// var age = 3;
 
- while(age < 10){
+//  while(age < 10){
 
-     document.write("Your age is less than 10 " +age);
-    age++;
+//      document.write("Your age is less than 10 " +age);
+//     age++;
 
-}
-
-document.write("you are more than 10years old")
-
-
-
-// for(age = 0; age < 10; age++){
-
-//      document.write("Your age is " +age);
-    
 // }
 
-//  document.write("you are now over 10");
+// document.write("you are more than 10years old")
+
+
+
+// // for(age = 0; age < 10; age++){
+
+// //      document.write("Your age is " +age);
+    
+// // }
+
+// //  document.write("you are now over 10");
+
+// let age = 5;
+ 
+// while (age <= 100) {
+//     console.log("Your age " + age + " is less than 10");
+//     age += age;
+// }
+// console.log (" you are now over 10")
+
+
+for(age = 5; age < 10; age++) {
+    console.log("Your age " + age + " is less than 10");
+    
+}
+console.log (" you are now over 10")

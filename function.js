7a9aee95f65136d98e05284329cfb83dcdf6e914
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-12 09:13:28 +0100

    update on function and loop-break

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 function getAverage (a,b,c,d,e,f) {
 
-    var average = (a + b + c + d + e + f) / 6;
+    var average = (a + b + c + d + e + f) / arguments.length;
     console.log(average);
     return average
 

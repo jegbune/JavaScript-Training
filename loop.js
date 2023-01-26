1ac1a7109f9f6783660ef36518bdaf05d6e3fa31
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-26 09:46:16 +0100

    javascript lesson on loop

--- loop.js
+++ loop.js
@@ -12,11 +12,38 @@
 // }
 // console.log(result)
 
-let cars = ["Audi","Toyota", "Honda", "BMW","KIA"];
-let i,len,text;
+// let cars = ["Audi","Toyota", "Honda", "BMW","KIA"];
+// let i,len,text;
 
-for(i = 0, len = cars.length, text = ""; i < len; i++) {
-    text += cars[i]; 
+// for(i = 0, len = cars.length, text = ""; i < len; i++) {
+//     text += cars[i]; 
 
+// }
+// console.log(text)
+
+// let i= 5;
+
+// for (let i = 0; i < 10; i++){
+
+// }
+// console.log(i);
+
+
+const person = {fname:"John", lname:"Doe", age:25};
+const person1 = ["ade", "shola", "wale","austin"]
+ let txt= "";
+ for(let x in person) {
+     txt += person[x];
+     
+ }
+console.log(txt);
+
+const numbers = [4,9,16,25,36,49]
+
+let fig = "";
+for(let x in numbers){
+    fig += numbers[x];
 }
-console.log(text)
+
+console.log(fig);
+console.log(typeof fig);

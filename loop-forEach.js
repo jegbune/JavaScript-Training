435cdf5df2da54f38a9f8338f252a63e21244b2e
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-30 00:20:40 +0100

    javascript lesson update on loop of

--- loop-forEach.js
+++ loop-forEach.js
@@ -1,7 +1,16 @@
-const myNumber = [23,12,45,65,34,3,90]
-let txt = "";
-myNumber.forEach(myfunction);
-console.log(txt)
-function myfunction(value, array, index){
-    txt += value; 
+// const myNumber = [23,12,45,65,34,3,90]
+// let txt = "";
+// myNumber.forEach(myfunction);
+// console.log(txt)
+// function myfunction(value, array, index){
+//     txt += value; 
+// }
+
+const cars = ["BMW", "Toyota", "Honda", "Audi", "Ferrari"];
+
+let car = "";
+
+for (let x of cars){
+    car += x +" ";
 }
+    console.log(car);

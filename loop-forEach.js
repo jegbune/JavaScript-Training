1a89317e5934dfb827571d3db9723702b220fae7
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-01-31 16:00:32 +0100

    javascript lesson loop for of

--- loop-forEach.js
+++ loop-forEach.js
@@ -1,11 +1,11 @@
-const myNumber = ["BMW", "Toyota", "Honda", "Audi", "Ferrari"];
-//  [23,12,45,65,34,3,90];
-let txt = "";
-myNumber.forEach(myfunction);
-console.log(txt)
-function myfunction(value, array, index){
-    txt += value; 
-}
+// const myNumber = ["BMW", "Toyota", "Honda", "Audi", "Ferrari"];
+// //  [23,12,45,65,34,3,90];
+// let txt = "";
+// myNumber.forEach(myfunction);
+// console.log(txt)
+// function myfunction(value, array, index){
+//     txt += value; 
+// }
 
 // const cars = ["BMW", "Toyota", "Honda", "Audi", "Ferrari"];
 
@@ -15,3 +15,16 @@ function myfunction(value, array, index){
 //     car += x +" ";
 // }
 //     console.log(car);
+
+
+
+
+let language = "javascript"
+
+let alphabet = ""
+
+for (let x of language){
+    alphabet += x +" ";
+
+}
+console.log(alphabet);

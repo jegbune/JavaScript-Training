91e40a50148e88c6833fce2c26b9b6aaf4a490b7
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-04 00:00:58 +0100

    javascript lesson on break and continue statement

--- break.js
+++ break.js
@@ -11,14 +11,27 @@
 // console.log(text)
 
 
+// let text = "";
+
+// for ( i= 0; i < 10; i++){
+//     if(i === 4 ){
+//         continue;
+//     }
+//         text += " This number " + i + " has jumped over";
+
+// }
+
+// console.log(text)
+
+
+const car = ["Acura", "Audi", "Mazda", "BMW", "chrysler"];
 let text = "";
-
-for ( i= 0; i < 10; i++){
-    if(i === 4 ){
-        continue;
-    }
-        text += " This number " + i + " has jumped over";
-
+list: {
+    text += car[0] + " ";
+    text += car[1] + " ";
+    break list;
+    text += car[2] + " ";
+    text += car[3] + " ";
+    text += car[4] + " ";
 }
-
-console.log(text)
+console.log (text);

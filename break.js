79b16f7bb74dc3b43069b42e1406e30154ecbee9
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-03 23:48:21 +0100

    javascript lesson on continue

--- break.js
+++ break.js
@@ -1,10 +1,23 @@
+// let text = "";
+
+// for ( i= 0; i < 10; i++){
+//     if(i === 4  ){
+//         break;
+//     }
+//         text += "This number " + i + " has jumped over";
+
+// }
+
+// console.log(text)
+
+
 let text = "";
 
 for ( i= 0; i < 10; i++){
-    if(i === 4  ){
-        break;
+    if(i === 4 ){
+        continue;
     }
-        text += "This number " + i + " has jumped over";
+        text += " This number " + i + " has jumped over";
 
 }
 

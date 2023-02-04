1947bd9f74773fd882ebeeca8bbb622a62153be3
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-04 19:58:21 +0100

    Javascript lesson on iterable js

---
+++ iterable.js
@@ -0,0 +1,25 @@
+// const School = "w3schools";
+
+// let text = "";
+// for(let a of School){
+//     text += a + "<br>";
+
+// }
+// console.log(text);
+
+// const School = ["a","b","c","d","e","f","g","h","j","k","l"];
+
+// let txt = "";
+// for(let x of School){
+//     txt += x + "<br>";
+// }
+// console.log(txt);
+
+
+const set = new Set(["a","b","c","d","e","f","g","h","j","k","l"])
+let text = "";
+
+for(let y of set){
+    text += y + "<br>"
+}
+console.log(text);

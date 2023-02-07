ce307a9cbbd7db68f08a8e7f8f10c72452ce1699
Jeremiah Ikechukwu <jegbune1@gmail.com>
2023-02-07 15:25:35 +0100

    javascript lesson on set forEach, iterable

--- set.js
+++ set.js
@@ -3,4 +3,14 @@ letter.add("e");
 let a ="f";
 // letter.delete("a");
 console.log (letter.size);
-console.log(letter);
+let text="";
+letter.forEach(function(value){
+    text += value;
+})
+console.log(text);
+console.log(letter.values());
+
+for(let x of letter.values()){
+    text += x + " <br>";
+}
+console.log(text)

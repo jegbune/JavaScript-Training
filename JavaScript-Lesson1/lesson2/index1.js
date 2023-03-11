73a692bc4189682937107b9b3a9fa20b03a1a56a
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-11 01:19:54 +0100

    javascript lesson on DOM

---
+++ JavaScript-Lesson1/lesson2/index1.js
@@ -0,0 +1,6 @@
+let errorMessage = document.getElementById('errormessage');
+
+function purchase() {
+    // alert('Someting went wrong')
+  errorMessage.textContent = 'Something went wrong, please try again';  
+}

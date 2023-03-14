2e87903a7e74daed2f3cde8db10b5eea88e528d5
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-15 00:50:12 +0100

    BlackCard Game

---
+++ JavaScript-Lesson1/BlackJackCard/script.js
@@ -0,0 +1,9 @@
+let age = 100;
+
+if(age < 100) {
+    console.log("Not eligible")
+} else if(age === 100) {
+    console.log("Here is your Birthday card from the King!")
+} else {
+    console.log("Not eligible, you have already gotten one")
+}

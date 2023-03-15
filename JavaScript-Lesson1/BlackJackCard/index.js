b97418384d9d478b2e6b1a8f6574d2836770b075
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-15 22:31:37 +0100

    Blackjack Game Update

--- JavaScript-Lesson1/BlackJackCard/index.js
+++ JavaScript-Lesson1/BlackJackCard/index.js
@@ -3,18 +3,31 @@ let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let messageEl = document.getElementById('message-el')
+// let sumEl = document.getElementById('sum-el')
+let sumEl = document.querySelector('#sum-el')
+let cardEl = document.querySelector('#card-el')
 
 let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-    message = "Wahoo! you've got Blackjack";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game!";
-    isAlive = false
+// startGame()
+
+function startGame() {
+
+    cardEl.textContent += " " + firstCard + " " +secondCard 
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Wahoo! you've got Blackjack";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false
+    }
+    
+    //CASH OUT
+    messageEl.textContent = message;
+
 }
 
-//CASH OUT
-console.log (message);

8f05c3ef41c59eb33b9be9355e3bc650367d55e4
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-16 02:02:59 +0100

    javascript array

---
+++ JavaScript-Lesson1/array.js
@@ -0,0 +1,10 @@
+let card = [7, 4];
+
+card.push(6);
+// console.log(card)
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, Thank you! How about you?",
+    "All good. Seen working on my portfolio lately."
+]

b87888d3cd87cecac117f2bf38755f09976d2e25
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-18 02:42:57 +0100

    BlackJack Game update

---
+++ JavaScript-Lesson1/BlackJackCard/script1.js
@@ -0,0 +1,10 @@
+let hasCompleteCourse = true;
+let givesCertificate = true;
+
+if (hasCompleteCourse === true && givesCertificate ===true) {
+        generateCertificate()
+ }
+
+function generateCertificate() {
+    console.log ("Generating Certificate....")
+}

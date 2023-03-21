fc2840522063de9a6542affc55d0d720afececf2
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-21 11:08:22 +0100

    update

--- JavaScript-Lesson1/BlackJackCard/script1.js
+++ JavaScript-Lesson1/BlackJackCard/script1.js
@@ -1,7 +1,4 @@
 let largeCountries = ["China","India","USA","indonesia","Pakistan"];
 
-console.log("The 5 Largest countries in the world:" )
-for(let i = 0; i <  largeCountries.length; i++ ) {
-    let country = largeCountries[i]
-    console.log("- " + country)
-}
+
+

2eeaec4df34559d38ba4d7de235be06905d47c8e
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-21 14:24:20 +0100

    update

--- JavaScript-Lesson1/BlackJackCard/script1.js
+++
@@ -1,8 +0,0 @@
-let hands = ["rock","paper", "scissor", "pen", "ruler"]; 
-
-function getRandomNumbers(){
-    let randomNumber = Math.floor(Math.random()*hands.length);
-   return (hands[randomNumber])
-}
-
-console.log(getRandomNumbers())

bf346e9597fa854e9e48ac6e339fa433285207cc
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-22 23:47:18 +0100

    update

--- JavaScript-Lesson1/lesson/index.js
+++ JavaScript-Lesson1/lesson/index.js
@@ -1,6 +1,11 @@
-const playerName = "per"
-let credits = 45
+const basePrice = 520
+const discount = 120
+let shippingCost = 12
+let shippingTIme = "5-12 days"
 
-credits = credits - 10
+shippingCost = 15
+shippingTIme ="7-14 days"
 
-console.log(credits)
+const fullPrice = basePrice - discount + shippingCost
+
+console.log("Total Cost: " + fullPrice + ". It will arrive in " + shippingTIme)

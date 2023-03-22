5a32f9976387452e395d613e458122c0989c7a77
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-22 23:31:28 +0100

    update

--- JavaScript-Lesson1/extension/extension1/index.js
+++ JavaScript-Lesson1/extension/extension1/index.js
@@ -1,4 +1,6 @@
- let inputBtn = document.getElementById("input-Btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el")
+let inputBtn = document.getElementById("input-Btn")
 
 inputBtn.addEventListener("click", function() {
     alert("Button clicked from addEventListener")

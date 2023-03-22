7fabc224fb8bec3a9724222c84aa3bfd8fc35127
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-23 00:32:22 +0100

    update

--- JavaScript-Lesson1/extension/extension1/index.js
+++ JavaScript-Lesson1/extension/extension1/index.js
@@ -3,8 +3,6 @@ const inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    // let myLeads1 = input.value
-    // myLeads.push("www.awesomelead.com")
-    // myLeads.push(myLeads1)
-    alert('yeads1')
+    myLeads.push("www.awesomelead.com")
+    alert(myLeads)
 })

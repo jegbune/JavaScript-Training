f7fb9ca60310d4a3e3ef32ef5953ae287ce952b6
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-23 10:46:44 +0100

    update

--- JavaScript-Lesson1/extension/extension1/index.js
+++ JavaScript-Lesson1/extension/extension1/index.js
@@ -1,15 +1,21 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
-} 
+function renderLeads() {
+
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    } 
+    ulEl.innerHTML = listItems
+}

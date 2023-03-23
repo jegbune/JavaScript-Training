72d542de6d9857d25dd970863fefd404ebe0f890
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-23 18:50:28 +0100

    update

--- JavaScript-Lesson1/extension/extension1/index.js
+++ JavaScript-Lesson1/extension/extension1/index.js
@@ -1,4 +1,4 @@
-let myLeads = [];
+let myLeads = [ ];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -12,7 +12,7 @@ function renderLeads() {
 
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a href='" + myLeads + ".com' target='_blank'> " + myLeads[i] + "</a></li>"
         // const li = document.createElement("li")
         // li.textContent = myLeads[i]
         // ulEl.append(li)

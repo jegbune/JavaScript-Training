6fb5c2dce8cb72474b6e2186007e730641679d90
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-03-24 16:09:22 +0100

    update

--- JavaScript-Lesson1/extension/extension4/index.js
+++ JavaScript-Lesson1/extension/extension4/index.js
@@ -1,7 +1,7 @@
 const recipient = "James"
 const sender = "Jeremiah"
 const email = `
-Hey ${recipient}! 
-How is it going? 
-Cheers ${sender}`
+    Hey ${recipient}! 
+    How is it going? 
+    Cheers ${sender}`
 console.log(email)
